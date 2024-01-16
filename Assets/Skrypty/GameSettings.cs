c1c742af0c7db727c0b15594efb46457922c0e9b
MichZack <michzack99@gmail.com>
2024-01-16 19:47:08 +0100

    Points added

--- Assets/Skrypty/GameSettings.cs
+++ Assets/Skrypty/GameSettings.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSettings : MonoBehaviour
 {
     private int result;
+    public Text txtScore;
 
     public void IncreaseScore()
     {
         result++;
+        txtScore.text = result.ToString();
     }
 
     public void EndGame()
     {
         Debug.Log("THE END");
+        
     }
 
 
